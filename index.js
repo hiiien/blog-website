9ad083daed0eddb5f99de3f2c97a9b7ed078e565
Gavin Ward <gavin.gbw@gmail.com>
2024-07-19 13:38:59 -0500

    fix delete and edit errors, change form submission

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const loadPosts = async () =>{
         return [];
     }
 }
-
 let postInfo;
 const init = async () => {
     postInfo = await loadPosts();
@@ -33,6 +32,7 @@ init();
 const savePosts = async (posts) =>{
     const dataJSON = JSON.stringify(posts);
     await fs.promises.writeFile(dataFilePath, dataJSON);
+    return await loadPosts();
 }
 
 function createPostContent(title = '', text = '', id = ''){
@@ -43,51 +43,47 @@ function createPostContent(title = '', text = '', id = ''){
     };
 }
 
-app.delete('/delete/:id', async (req, res) => {
-    const id = parseInt(req.params.id, 10); 
+app.post('/delete', async (req, res) => {
+    const id = req.body.id * 1; 
     const postToDelete = postInfo.find(post => post.id === id);
 
     if(!postToDelete){
-        return res.status(404).json({
+        return res.status(404).send({
             status: 'fail',
-            message: 'no movie object with id of ' + id + ' is found'
+            message: 'pose with id ' + id + ' not found'
         })
     }
-
     const index = postInfo.indexOf(postToDelete);
     postInfo.splice(index, 1);
-    await savePosts(postInfo);
-
-   res.status(200).json({
-    status: 'success',
-    message: 'Post deleted successfully',
-   });
+    const posts = await savePosts(postInfo);
+    res.render("index.ejs", {
+        posts: posts
+    });
 });
 
-app.put("/edit/:id", async (req, res) => {
-    const id = parseInt(req.params.id, 10);
+app.post("/edit", async (req, res) => {
+    const id = req.body.id * 1;
     const title = req.body["title"];
     const text = req.body["text"];
     console.log('Title:', title);
     console.log('Text:', text); 
     const postToEdit = postInfo.find(post => post.id === id);
     if(!postToEdit){
-        return res.status(404).json({
+        return res.status(404).send({
             status: 'fail',
-            message: 'no movie object with id of ' + id + ' is found'
+            message: 'post with id ' + id + ' not found'
         })
     }
-    
     const index = postInfo.indexOf(postToEdit);
     postInfo[index].title = title;
     postInfo[index].text = text;
     await savePosts(postInfo);
-
-    res.status(200).json({
-        status: 'success',
-        message: 'Post updated successfully',
-        post: postInfo 
-    });
+    postToEdit.title = title;
+    postToEdit.text = text;
+    const posts = await savePosts(postInfo);
+    res.render('index.ejs', {
+        posts: posts
+    })
 })
 
 app.post("/submit-form", async (req, res) => {
@@ -96,28 +92,20 @@ app.post("/submit-form", async (req, res) => {
     console.log(postText, postTitle);
     if(postTitle === '' || postText === ''){
         console.log("Empty title or text, not adding to postInfo");
-        return res.status(400).json({
+        return res.status(400).send({
             status: 'fail',
             message: 'Title and text cannot be empty'
         })
     }
-
     let postId = postInfo.length === 0 ? 1 : postInfo[postInfo.length - 1].id + 1;
-
     const newPost = (createPostContent(postTitle, postText, postId));
     if(newPost.text && newPost.text){
-        //try to get rid of the double submit issue
-
-        //try to change code to send post object to the front-end
-        //so it can preappend a post without have to rerender the whole page
             postInfo.push(newPost);
             await savePosts(postInfo);
             res.redirect("/");
     }
  
 });
-
-
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,51 +11,6 @@ function toggleEditForm(postId) {
     }
 }
 
-
-document.querySelector('#myForm').addEventListener('submit', async (e) =>{
-    //try to get e.preventDefault
-    try {
-        let response = await fetch('/submit-form', {
-            method: 'POST',
-            body: new FormData(document.getElementById('myForm'))
-        })
-
-        if (response.ok) {
-            console.log('Form submitted successfully:', await response.json());
-            document.querySelector("#myForm").reset();
-        } else {
-            console.log('Form submission failed:', response.status, await response.text());
-        }
-        reload();
-    } catch (error) {
-        console.log(`ERROR: ${error}`);
-    }
-});
-
-
-document.querySelectorAll('.delete').forEach(button => {
-    button.addEventListener('click', async (e) => {
-        e.preventDefault();
-        const id = e.target.dataset.id;
-        try {
-            let response = await fetch(`/delete/${id}`, {
-                method: 'DELETE',
-                headers: {
-                    'content-type': 'application/json',
-                },
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-        } catch (error) {
-            console.log('Error deleting:', error);
-        }
-    });
-});
-
-
 document.querySelectorAll(".edit").forEach(button =>{
     button.addEventListener('click', (e) => {
         toggleEditForm(e.target.dataset.id);
@@ -68,22 +23,8 @@ document.querySelectorAll(".cancel-change").forEach(button => {
     })
 });
 
-document.querySelectorAll(".post > form").forEach(button => {
+document.querySelectorAll(".form-edit").forEach(button => {
     button.addEventListener('submit', async (e) =>{
-        const id = e.target.querySelector('.edit-submit').dataset.id;
-        try{
-            const response = await fetch (`/edit/${id}`, {
-                method: 'PUT',
-                body: new FormData(e.target)
-            })
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-
-        } catch (error) {
-            console.error('Fetch error:', error);
-            res.status(500).send('Internal Server Error');
-          }
-          toggleEditForm(id);
+          toggleEditForm(e.target.dataset.id);
     })
 })
